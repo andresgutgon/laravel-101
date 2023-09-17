c1fe28ab0bf0a9f807eefd3d8b0f3a59555ca654
andresgutgon <andresgutgon@gmail.com>
2023-09-17 12:01:20 +0200

    Deleting chips

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -35,6 +35,7 @@ class ChirpPolicy {
      * Determine whether the user can delete the model.
      */
     public function delete(User $user, Chirp $chirp): bool {
+        return $this->update($user, $chirp);
     }
 
     /**
